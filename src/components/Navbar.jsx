38dfd3f3a96f40c1f3ba6381ac02cce73b83ad8a
naseem1999 <naseemmuhammad700@gmail.com>
2025-05-29 13:03:09 +0500

    use-effect hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { memo } from 'react'
-const Navbar = ({adjective,getAdjective}) => {
-    console.log("navbar is rendered")
+import React, { useEffect } from 'react'
+const Navbar = ({color}) => {
+    //run on every render
+    useEffect(()=>{
+        alert("Hey i will run on every Render child")
+     })
+     //run on first render
+     useEffect(()=>{
+        alert("this is first render")
+     },[])
+     //run on color change
+    useEffect(()=>{
+        alert("I am running because ciolor was changed")
+    },[color])
+    useEffect(()=>{
+        alert("this is the first render of App.jsx")
+        return ()=>{
+            alert("component was unmounted")
+        }
+     },[])
   return (
     <div>
-        I am {adjective} Navbar
-        <button onClick={()=>{getAdjective()}}>{getAdjective()}</button>
+      I am a Navbar of {color} color hehe...
     </div>
   )
 }
 
-export default memo(Navbar)
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import App1 from './App1.jsx'
+import App2 from './App2.jsx'
 createRoot(document.getElementById('root')).render(
   
-    <App1 />
+    <App2 />
  
 )
